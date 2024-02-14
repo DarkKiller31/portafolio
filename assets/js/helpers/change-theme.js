5c789614a8242452af65c366bdabce6bd40fa71d
JuanCTrujillo <150824287+DarkKiller31@users.noreply.github.com>
2024-02-14 00:19:11 -0500

    Update change-theme.js

--- assets/js/helpers/change-theme.js
+++ assets/js/helpers/change-theme.js
@@ -5,17 +5,3 @@ btnSwitch.addEventListener('click', () => {
     btnSwitch.classList.toggle('active');
 });
 
-
-
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)')) {
-    document.body.classList.add('dark');
-} else {
-    document.body.classList.remove('light');
-}
-// window.matchMedia('(prefers-color-scheme: dark)')
-//     .addEventListener('change', event => {
-//         document.body.classList.add('dark');
-//     })
-
-
-
